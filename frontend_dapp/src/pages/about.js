1bf86fc014b2620eb222024c0e2707efa6a7f203
helios9284 <colonelme9284@gmail.com>
2025-11-18 06:49:39 -0500

    add:Integrate rainbow-kit and wagmi

---
+++ frontend_dapp/src/pages/about.js
@@ -0,0 +1,13 @@
+
+import { Inter } from 'next/font/google'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function About() {
+  return (
+    <div className="">
+      <p>This is the about us page</p>
+    </div>
+  )
+}
