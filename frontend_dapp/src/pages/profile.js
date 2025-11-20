37494d419b4162d8679c85fb479f6dd1e2ab14ad
helios9284 <colonelme9284@gmail.com>
2025-11-20 06:44:27 -0500

    add: profile component

--- frontend_dapp/src/pages/profile.js
+++ frontend_dapp/src/pages/profile.js
@@ -1,12 +1,17 @@
 
 import { Inter } from 'next/font/google'
+import Header from '../../src/components/profile/Header';
+import Detail from '../../src/components/profile/Detail';
+import Market from '../../src/components/profile/Market';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Profile() {
   return (
     <div className="">
-      <p>This is the profile page</p>
+      <Header />
+      <Detail />
+      <Market />
     </div>
   )
 }
